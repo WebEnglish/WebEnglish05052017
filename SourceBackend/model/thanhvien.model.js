40bcabfe787d2f75d77920fcac285474c0e460ab
lehung22297 <44271319+lehung22297@users.noreply.github.com>
2019-06-11 15:42:36 +0700

    Cài đặt chức năng đăng nhập phân quyền, năng quản lí từ vựng, xem profile người dùng

--- SourceBackend/model/thanhvien.model.js
+++ SourceBackend/model/thanhvien.model.js
@@ -1,11 +1,37 @@
 var db = require('../utils/Database');
 module.exports = {
+
   add: entity => {
     return db.add('taikhoan', entity);
   },
   
   getPassbyEmail: email =>{
-    return db.load(`select * from taikhoan where email = '${email}'`);
-
+    return db.load(`select * from taikhoan where email = '${email}' and Xoa = 0`);
   },
+
+  all: () => {
+    return db.load('select * from taikhoan where Xoa = 0');
+  },
+
+  getListbyPH: id =>{
+    return db.load(`select * FROM phanhe as ph , taikhoan as tk where tk.Xoa = 0 and ph.idPhanHe = '${id}' and tk.phanhe = ph.idPhanHe`  )
+  },
+
+  delete: id => {
+    return db.delete('taikhoan', 'idTaiKhoan', id);
+  }
+  ,
+  update: entity => {
+    return db.update('taikhoan', 'idTaiKhoan', entity);
+  },
+
+  GetAllById: id => {
+    return db.load(`select * from taikhoan where idTaiKhoan = '${id}' and Xoa = 0`);
+  },
+
+  getIDByEmail: email => {
+    return db.load(`select * from taikhoan where email = '${email}' and Xoa = 0`);
+  }
+
+
 };
---
+++ SourceBackend/router/admin-router/QLTaiKhoan.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+var passport = require('passport');
+var userModel = require('../../model/thanhvien.model')
+
+// router.get('/delete/:id',(req,res,next) => {
+//     var id = req.params.id;
+//     res.redirect('/admin/taikhoan/delete');
+// })
+
+router.get('/delete/:id', (req, res, next) => {
+    var id = req.params.id
+    var entity = {
+        idTaiKhoan: id,
+        Xoa: 1
+    }
+    userModel.update(entity).then(n => {
+        res.redirect('/admin/taikhoan');
+    }).catch(err => {
+        console.log(err);
+        res.end('error occured.')
+    });
+})
+
+router.get('/edit/:id', (req, res, next) => {
+    var id = req.params.id;
+    userModel.GetAllById(id).then(tk => {
+        res.render('admin/TaiKhoan/EditTaiKhoan', {
+            member: tk[0],
+            layout: './indexAdmin'
+
+        }).catch(err => {
+            console.log(err);
+            res.end('error occured.')
+        });
+        console.log(member);
+    })
+
+})
+
+router.get('/edit/:id', (req, res, next) => {
+    var id = req.params.id;
+    userModel.GetAllById(id).then(tk => {
+        res.render('admin/TaiKhoan/EditTaiKhoan', {
+            member: tk[0],
+            layout: './indexAdmin'
+
+        }).catch(err => {
+            console.log(err);
+            res.end('error occured.')
+        });
+        console.log(member);
+    })
+
+})
+
+router.post('/edit', (req, res, next) => {
+    var temp = req.body;
+    userModel.getIDByEmail(temp.email).then(row => {
+        var entity = {
+            idTaiKhoan: row[0].idTaiKhoan,
+            hoten: temp.ten,
+            email: temp.email,
+            ngaysinh: temp.date,
+            NgayTaoTK: temp.da
+        }
+        userModel.update(entity).then(id => {
+            res.redirect('/admin/taikhoan')
+        })
+    })
+
+})
+
+module.exports = router;
