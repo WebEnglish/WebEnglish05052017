62e982000286772072b57aed5ecad726f6d48cbe
VietAn98 <44429962+VietAn98@users.noreply.github.com>
2019-06-11 03:17:28 -0700

    Delete writer.mdw.js

--- SourceBackend/middlewares/writer.mdw.js
+++
@@ -1,9 +0,0 @@
-var writerModel = require('../model/Forwriter');
-
-module.exports = (req, res, next) => {
-    writerModel.menu()
-  .then(rows => {     
-    res.locals.Action = rows;
-    next();
-  })
-}
